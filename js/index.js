8705874e2f77aec0ecb49e94c589bce6db1b100a
Quốc Hân <dvqhan@gmail.com>
2016-06-08 01:40:36 +0700

    angularFire, database in firebase

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
-'use strict';
+var app = angular.module('MyMobileStore', ['ui.bootstrap', 'ngRoute', 'firebase']);
+app.controller('DataController', ['$scope','$http','$firebaseObject', function($scope, $http, $firebaseObject)
+{
+    var ref = new Firebase('https://finalproject-mobileshop.firebaseio.com/');
+    $scope.data = $firebaseObject(ref);
+    $scope.data.$loaded()
+        .then(function() {
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+            function ref(data){
+                var s="";
+                data.forEach(function(entry){s+=entry.content+"\n";});
+                return s;
+            }
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+
+
+            console.log($scope.data);
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+
 }]);
--- js/load-data.js
+++ js/load-data.js
@@ -1,30 +1,27 @@
 /**
  * Created by dvqha_000 on 6/2/2016.
  */
-angular.module('MyApp', ['ngSanitize','firebase'])
-    .controller('ExampleController', ['$scope','$http','$firebaseObject', function($scope, $http, $firebaseObject)
-    {
-        var ref = new Firebase('https://finalproject-mobileshop.firebaseio.com/');
-        $scope.data = $firebaseObject(ref);
-        $scope.data.$loaded()
-            .then(function() {
+var app = angular.module('MyMobileStore', ['ngSanitize','firebase']);
 
-                function ref(data){
-                    var s="";
-                    data.forEach(function(entry){s+=entry.content+"\n";});
-                    return s;
-                };
+app.controller('DataController', ['$scope','$http','$firebaseObject', function($scope, $http, $firebaseObject)
+{
+    var ref = new Firebase('https://finalproject-mobileshop.firebaseio.com/');
+    $scope.data = $firebaseObject(ref);
+    $scope.data.$loaded()
+        .then(function() {
 
-                $scope.category=ref($scope.data.category);
-                $scope.header=ref($scope.data.info-header);
-                $scope.footer=ref($scope.data.info-footer);
-                $scope.team=ref($scope.data.info-team);
-                $scope.products=ref($scope.data.products);
+            function ref(data){
+                var s="";
+                data.forEach(function(entry){s+=entry.content+"\n";});
+                return s;
+            }
 
-                console.log($scope.data);
-            })
-            .catch(function(err) {
-                console.error(err);
-            });
 
-    }]);
+
+            console.log($scope.data);
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+
+}]);
